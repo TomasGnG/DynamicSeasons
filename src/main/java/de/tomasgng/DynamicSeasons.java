30f446f80aa4c3a1a08e1d734d9348ddc1d41415
Tomas K <tomaskeder2005@gmail.com>
2023-07-20 18:17:31 +0200

    integrated bStats

--- src/main/java/de/tomasgng/DynamicSeasons.java
+++ src/main/java/de/tomasgng/DynamicSeasons.java
@@ -7,12 +7,16 @@ import de.tomasgng.listeners.ThunderChangeListener;
 import de.tomasgng.listeners.WeatherChangeListener;
 import de.tomasgng.placeholders.CurrentSeasonExpansion;
 import de.tomasgng.placeholders.DurationExpansion;
+import de.tomasgng.utils.bstats.Metrics;
 import de.tomasgng.utils.managers.ConfigManager;
 import de.tomasgng.utils.managers.MessageManager;
 import de.tomasgng.utils.managers.SeasonManager;
 import lombok.Getter;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public final class DynamicSeasons extends JavaPlugin {
 
@@ -44,6 +48,14 @@ public final class DynamicSeasons extends JavaPlugin {
         manager.registerEvents(new WeatherChangeListener(), this);
 
         getCommand("dynamicseasons").setExecutor(new DynamicSeasonsCMD());
+
+        Metrics metrics = new Metrics(this, 19158);
+        metrics.addCustomChart(new Metrics.MultiLineChart("players_and_servers", () -> {
+            Map<String, Integer> valueMap = new HashMap<>();
+            valueMap.put("servers", 1);
+            valueMap.put("players", Bukkit.getOnlinePlayers().size());
+            return valueMap;
+        }));
 
         if(Bukkit.getPluginManager().isPluginEnabled("PlaceholderAPI")) {
             new DurationExpansion().register();
