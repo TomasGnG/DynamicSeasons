d9e424b131db89dddd0a8c36bc0bcc5f3b7a8632
TomasGnG <tomaskeder2005@gmail.com>
2023-10-20 21:31:54 +0200

    optimized code and sending the player a message when 0 worlds are loaded

--- src/main/java/de/tomasgng/listeners/PlayerJoinListener.java
+++ src/main/java/de/tomasgng/listeners/PlayerJoinListener.java
@@ -9,17 +9,22 @@ import org.bukkit.event.player.PlayerJoinEvent;
 
 public class PlayerJoinListener implements Listener {
 
-    private MiniMessage mm = MiniMessage.miniMessage();
+    private final MiniMessage mm = MiniMessage.miniMessage();
 
     @EventHandler
     public void on(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+
         if(!event.getPlayer().isOp())
             return;
-        Player player = event.getPlayer();
+
         if(DynamicSeasons.getInstance().updateCheck(true) != null) {
             player.sendMessage(mm.deserialize("<gradient:#55C156:#FFFF00:#FFA500:#87CEFA>DynamicSeasons</gradient> <dark_gray>| <yellow>Using an outdated version(" + DynamicSeasons.getInstance().getDescription().getVersion() + "). Newest version " + DynamicSeasons.getInstance().updateCheck(true)));
             player.sendMessage(mm.deserialize("<gradient:#55C156:#FFFF00:#FFA500:#87CEFA>DynamicSeasons</gradient> <dark_gray>| <yellow><hover:show_text:'<green>Clicking this message will update the plugin.'><click:run_command:/dynamicseasons update>Click here to update the plugin</click>."));
         }
+
+        if(DynamicSeasons.getInstance().isPLUGIN_DISABLED())
+            player.sendMessage(DynamicSeasons.getInstance().getDISABLED_MESSAGE());
     }
 
 }
